1054eb76369c07c9e8485dd16dc5ec52d2359598
Michel Blanc <mb@mbnet.fr>
2013-03-08 18:04:54 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+i<!doctype html>
+<html>
+  <head>
+    <title>Awesome app</title>
+  </head>
+  <body>
+  <h1>Welcome to your awesome app !</h1>
+
+App backend <?=gethostname()?>
+
+    <img src="http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg" />
+  </body>
+</html>
+
+
