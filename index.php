5ad93ede38c87a4e49684a4776a7aee73007976a
Michel Blanc <mb@mbnet.fr>
2013-03-08 18:49:20 +0100

    Adds para

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <body>
   <h1>Welcome to your awesome app !</h1>
 
-App backend <?=gethostname()?>
+  <p> App backend <?=gethostname()?> </p>
 
     <img src="http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg" />
   </body>
