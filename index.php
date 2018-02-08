2cec0953e2fc0eefbcc58b178a144ce60bd76b43
Dan Keller <dan@dankeller.org>
2018-02-08 07:53:00 -0600

    replace kitten with Murray

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
   <p> App backend <?=gethostname()?> </p>
 
-    <img src="http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg" />
+    <img src="http://fillmurray.com/408/287" />
   </body>
 </html>
 
